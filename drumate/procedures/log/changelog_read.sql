f6026909c9b57deb8f3e74517846b5a074dbf9a1
Somanos Sar <somanos@drumee.com>
2024-11-25 10:18:15 +0100

    Changed bin get defult collation febore applying patches

---
+++ drumate/procedures/log/changelog_read.sql
@@ -0,0 +1,11 @@
+DELIMITER $
+
+DROP PROCEDURE IF EXISTS `changelog_read`$
+-- CREATE PROCEDURE `changelog_read`(
+--   IN _id INT(10)
+-- )
+-- BEGIN
+--   SELECT *  FROM changelog WHERE id >= _id;
+-- END$
+
+DELIMITER ;
