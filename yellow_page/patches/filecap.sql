7d2f4882401864be66dc05073bd0d406767c8a2f
Somanos Sar <somanos@drumee.com>
2025-02-07 15:51:09 +0700

    Changed filecap handle double exention

--- yellow_page/patches/filecap.sql
+++ yellow_page/patches/filecap.sql
@@ -2,8 +2,9 @@ replace into filecap select null, 'drumee.json', 'skeleton', 'application/json',
 replace into filecap select null, 'skl.json', 'skeleton', 'application/json', '---', 'Drumee skeleton data';
 replace into filecap select null, 'poll.json', 'form', 'application/json', '---', 'Drumee form data';
 replace into filecap select null, 'form.json', 'form', 'application/json', '---', 'Drumee form data';
-replace into filecap select null, 'd.ts.map', 'text', 'plain/tex', '---', 'Typescript map file';
-replace into filecap select null, 'scss.map', 'text', 'plain/tex', '---', 'Scss map file';
-replace into filecap select null, 'js.map', 'text', 'plain/tex', '---', 'Javascript map file';
-replace into filecap select null, 'd.ts', 'text', 'plain/tex', '---', 'Typescript ';
+replace into filecap select null, 'd.ts.map', 'text', 'text/plain', '---', 'Typescript map file';
+replace into filecap select null, 'scss.map', 'text', 'text/plain', '---', 'Scss map file';
+replace into filecap select null, 'js.map', 'text', 'text/plain', '---', 'Javascript map file';
+replace into filecap select null, 'd.ts', 'text', 'text/plain', '---', 'Typescript ';
+replace into filecap select null, 'drumee.html', 'note', 'text/html', '---', "Drumee note";
 update filecap set capability='---' where capability='r';
