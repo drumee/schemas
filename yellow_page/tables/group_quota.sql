9a0c5667f2a5e212d6f35c138fa12a2b2785b7b9
Somanos Sar <somanos@drumee.com>
2025-09-18 10:13:50 +0200

    Changed grpup_quota

--- yellow_page/tables/group_quota.sql
+++ yellow_page/tables/group_quota.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `group_quota` (
+CREATE TABLE IF NOT EXISTS `group_quota` (
   `category` VARCHAR(512) DEFAULT "regular",
   `private_hub` VARCHAR(16) DEFAULT "Infinity",
   `share_hub` VARCHAR(16) DEFAULT "Infinity",
@@ -9,3 +9,12 @@ CREATE TABLE `group_quota` (
   `conference` VARCHAR(32) DEFAULT "Infinity",
   PRIMARY KEY (`category`)
 ) ENGINE=InnoDB DEFAULT CHARSET=ascii COLLATE=ascii_general_ci;
+
+LOCK TABLES `group_quota` WRITE;
+INSERT IGNORE INTO `group_quota` VALUES 
+  ('guest','10','10','0','2147483648','1073741824','0','0'),
+  ('default','Infinity','Infinity','Infinity','Infinity','Infinity','0','Infinity'),
+  ('regular','Infinity','Infinity','Infinity','Infinity','Infinity','Infinity','Infinity'),
+  ('sandbox','10','10','2','209715200','209715200','0','10'),
+  ('system','0','0','0','0','0','0','0');
+UNLOCK TABLES;
