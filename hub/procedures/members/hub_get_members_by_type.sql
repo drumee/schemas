88feb3e0d14fc2ebe26c169cabdf974830309919
Somanos Sar <somanos@drumee.com>
2026-01-10 00:32:56 +0100

    Changed hub_get_members_by_type

--- hub/procedures/members/hub_get_members_by_type.sql
+++ hub/procedures/members/hub_get_members_by_type.sql
@@ -9,28 +9,32 @@ CREATE PROCEDURE `hub_get_members_by_type`(
 BEGIN
   DECLARE _range bigint;
   DECLARE _offset bigint;
+  DECLARE _ts bigint UNSIGNED;
   DECLARE _drumate_db VARCHAR(400);
-    CALL pageToLimits(_page, _offset, _range);
 
-    SELECT db_name  FROM yp.entity WHERE id = _uid INTO _drumate_db;
+  SELECT UNIX_TIMESTAMP() INTO _ts;
 
-    DROP TABLE IF EXISTS _contact;
-    CREATE TEMPORARY TABLE _contact AS
-    SELECT  
-      _page as `page`, 
-      d.id,
-      permission AS privilege,
-      d.email,
-      d.firstname  firstname,
-      d.lastname  lastname,
-      d.fullname  fullname,
-      d.fullname  surname ,
-      -- TRIM(IFNULL(JSON_UNQUOTE(JSON_EXTRACT(profile, '$.firstname')), ''))AS firstname,
-      -- TRIM(IFNULL(JSON_UNQUOTE(JSON_EXTRACT(profile, '$.lastname')), '')) AS lastname
-      JSON_VALUE(d.profile, "$.connected") `online`
-    FROM permission p 
-    INNER JOIN yp.drumate d on p.entity_id=d.id 
-    LEFT JOIN yp.socket s on s.uid=d.id
+  CALL pageToLimits(_page, _offset, _range);
+
+  SELECT db_name  FROM yp.entity WHERE id = _uid INTO _drumate_db;
+
+  DROP TABLE IF EXISTS _contact;
+  CREATE TEMPORARY TABLE _contact AS SELECT  
+    _page as `page`, 
+    d.id,
+    permission AS privilege,
+    d.email,
+    d.firstname  firstname,
+    d.lastname  lastname,
+    d.fullname  fullname,
+    d.fullname  surname ,
+    expiry_time expiry,
+    IF(expiry_time, TIMESTAMPDIFF(DAY, FROM_UNIXTIME(_ts), FROM_UNIXTIME(expiry_time)), 0) AS days,
+    IF(expiry_time, MOD(TIMESTAMPDIFF(HOUR, FROM_UNIXTIME(_ts), FROM_UNIXTIME(expiry_time) ), 24), 0) AS hours,
+    IF(s.id IS NOT NULL, 1, 0) `online`
+  FROM permission p 
+    INNER JOIN yp.drumate d ON p.entity_id=d.id 
+    LEFT JOIN yp.socket s ON s.uid=d.id
     WHERE 
       resource_id='*'  AND 
       CASE 
@@ -41,32 +45,41 @@ BEGIN
         ELSE 1
       END = 1;
 
-     -- ALTER TABLE _contact ADD surname  varchar(255);
-       ALTER TABLE _contact ADD contact_id  varchar(16);
-       ALTER TABLE _contact ADD status  varchar(16);
+  ALTER TABLE _contact ADD contact_id  varchar(16);
+  ALTER TABLE _contact ADD status  varchar(16);
 
+  SET @st = CONCAT(" UPDATE ",_drumate_db, ".contact c
+    INNER JOIN ",_drumate_db, ".contact_email ce ON ce.contact_id = c.id   AND ce.is_default = 1  
+    INNER JOIN _contact tc ON tc.id = c.uid OR tc.id = c.entity OR tc.email = ce.email
+    SET tc.contact_id  = c.id, 
+        tc.status = c.status,
+        tc.firstname = c.firstname,  
+        tc.lastname  = c.lastname,
+        tc.surname   = IFNULL(c.surname,IF(coalesce(c.firstname, c.lastname) IS NULL,ce.email,CONCAT( IFNULL(c.firstname, '') ,' ',  IFNULL(c.lastname, '')))) ,
+        tc.fullname  = CONCAT(IFNULL(c.firstname, ''), ' ', IFNULL(c.lastname, '')) 
+      "); 
+  PREPARE stmt FROM @st;
+  EXECUTE stmt;
+  DEALLOCATE PREPARE stmt;
 
-
-    SET @st = CONCAT(" UPDATE ",_drumate_db, ".contact c
-      INNER JOIN ",_drumate_db, ".contact_email ce ON ce.contact_id = c.id   AND ce.is_default = 1  
-      INNER JOIN _contact tc ON tc.id = c.uid OR tc.id = c.entity OR tc.email = ce.email
-      SET tc.contact_id  = c.id, 
-          tc.status = c.status,
-          tc.firstname = c.firstname,  
-          tc.lastname  = c.lastname,
-          tc.surname   = IFNULL(c.surname,IF(coalesce(c.firstname, c.lastname) IS NULL,ce.email,CONCAT( IFNULL(c.firstname, '') ,' ',  IFNULL(c.lastname, '')))) ,
-          tc.fullname  = CONCAT(IFNULL(c.firstname, ''), ' ', IFNULL(c.lastname, '')) 
-        "); 
-    PREPARE stmt FROM @st;
-    EXECUTE stmt;
-    DEALLOCATE PREPARE stmt;
-
-    SELECT page,id,privilege,email,lastname,fullname,surname, `online`,firstname, 1 as is_drumate, status, contact_id
-    -- Temp fix - shall be updated upon ui pushed into production
-    -- IF(firstname IS NULL AND lastname IS NULL, email, fullname) firstname 
-    FROM _contact
-    GROUP BY email
-    ORDER BY firstname ASC, id ASC; -- LIMIT _offset, _range;  
+  SELECT page,
+    id,
+    privilege,
+    email,
+    lastname,
+    fullname,
+    surname, 
+    `online`,
+    firstname, 
+    1 as is_drumate, 
+    status, 
+    contact_id,
+    expiry,
+    days,
+    hours
+  FROM _contact
+  GROUP BY email
+  ORDER BY firstname ASC, id ASC; -- LIMIT _offset, _range;  
 
 END$
 
