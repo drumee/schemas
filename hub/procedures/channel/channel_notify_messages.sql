63939e2a9084d1a621d301528dce8719362957b7
duynguyen <duynguyen@local.drumee>
2026-04-13 06:12:34 -0700

    Add mention_cnt and share_cnt to channel_notify_messages

--- hub/procedures/channel/channel_notify_messages.sql
+++ hub/procedures/channel/channel_notify_messages.sql
@@ -1,4 +1,3 @@
-
 DELIMITER $
 -- ==============================================================
 -- 
@@ -9,10 +8,15 @@ CREATE PROCEDURE `channel_notify_messages`(
 )
 BEGIN
   SELECT 
-    COUNT(1)  read_cnt
+    COUNT(1) read_cnt,
+    SUM(CASE WHEN JSON_SEARCH(mention_ids, 'one', _uid) IS NOT NULL THEN 1 ELSE 0 END) mention_cnt,
+    SUM(CASE
+      WHEN is_forward = 1
+        OR (attachment IS NOT NULL AND LTRIM(RTRIM(attachment)) != '' AND attachment != 'null')
+      THEN 1 ELSE 0
+    END) share_cnt
   FROM channel WHERE 
   JSON_EXISTS(metadata, "$._seen_") AND  
   NOT JSON_EXISTS(metadata, CONCAT("$._seen_.", _uid)); 
 END$
-
 DELIMITER ;
