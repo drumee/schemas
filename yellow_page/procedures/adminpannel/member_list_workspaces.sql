df3ead487ed367abd92f09e3701806d498c5fae6
duynguyen <duynguyen@local.drumee>
2026-05-02 09:48:34 -0700

    Small fix

--- yellow_page/procedures/adminpannel/member_list_workspaces.sql
+++ yellow_page/procedures/adminpannel/member_list_workspaces.sql
@@ -3,33 +3,32 @@ DELIMITER $
 DROP PROCEDURE IF EXISTS `member_list_workspaces`$
 CREATE PROCEDURE `member_list_workspaces`(
   IN _uid VARCHAR(16),
-  IN _org_id VARCHAR(16)
+  IN _dom_id INT
 )
 BEGIN
-  DECLARE _dom_id INT;
   DECLARE _db_name VARCHAR(255) CHARACTER SET ascii;
 
-  SELECT domain_id FROM organisation WHERE id = _org_id INTO _dom_id;
   SELECT db_name FROM entity WHERE id = _uid INTO _db_name;
 
   IF _db_name IS NULL THEN
-    SELECT NULL AS hub_id, NULL AS hub_name LIMIT 0;
+    SELECT NULL AS hub_id, NULL AS hub_name, NULL AS permission LIMIT 0;
   ELSE
     SET @sql = CONCAT(
-      'SELECT h.id AS hub_id, h.name AS hub_name ',
+      'SELECT e.id AS hub_id, e.ident AS hub_name, p.permission ',
       'FROM `', _db_name, '`.permission p ',
-      'INNER JOIN yp.hub h ON h.id = p.resource_id ',
       'INNER JOIN yp.entity e ON e.id = p.resource_id ',
       'WHERE p.entity_id = ', QUOTE(_uid),
+      ' AND p.resource_id != \'*\' ',
       ' AND (p.expiry_time = 0 OR p.expiry_time > UNIX_TIMESTAMP()) ',
-      ' AND h.domain_id = ', _dom_id,
-      ' AND e.status = ''active'' ',
-      ' ORDER BY h.name ASC'
+      ' AND e.type = \'hub\' ',
+      ' AND e.dom_id = ', _dom_id,
+      ' AND e.status = \'active\' ',
+      ' ORDER BY e.ident ASC'
     );
     PREPARE stmt FROM @sql;
     EXECUTE stmt;
     DEALLOCATE PREPARE stmt;
   END IF;
-END $
+END$
 
 DELIMITER ;
