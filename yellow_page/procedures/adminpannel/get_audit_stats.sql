3cb45991af604693b5fa1561c21743b3d3730cb4
duynguyen <duynguyen@local.drumee>
2026-05-02 01:21:56 -0700

    New SP to get audit stats

---
+++ yellow_page/procedures/adminpannel/get_audit_stats.sql
@@ -0,0 +1,25 @@
+DELIMITER $
+
+DROP PROCEDURE IF EXISTS `get_audit_stats`$
+CREATE PROCEDURE `get_audit_stats`(
+  IN _domain_id INT(11) UNSIGNED,
+  IN _from_time INT(11),
+  IN _to_time INT(11)
+)
+BEGIN
+  DECLARE _storage_used FLOAT DEFAULT 0;
+
+  SELECT COALESCE(SUM(e.space), 0)
+  INTO _storage_used
+  FROM yp.entity e
+  WHERE e.dom_id = _domain_id
+    AND e.type = 'hub'
+    AND e.status = 'active';
+
+  SELECT
+    94 AS security_score,
+    0 AS high_risk_count,
+    _storage_used AS storage_used_bytes;
+END$
+
+DELIMITER ;
